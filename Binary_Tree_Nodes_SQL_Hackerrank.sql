9fb2bce98cd318a8f7356a27efbfd0ecdacac5e7
Anil Vignesh <anilvignesh@outlook.com>
2020-04-30 14:22:33 +0530

    Update Binary_Tree_Nodes_SQL_Hackerrank.sql
    formating

--- Binary_Tree_Nodes_SQL_Hackerrank.sql
+++ Binary_Tree_Nodes_SQL_Hackerrank.sql
@@ -9,9 +9,6 @@
 
 //Solution :  We are given a table BST with two columns, N(node), P(parent).
 
-
-
-
 select DISTINCT(N),//The logic here is simple, N lists all the nodes in the tree, so we need to take the distinct nodes
      CASE WHEN P IS NULL THEN "Root" //Now for classification, for a node if the parent is NULL it is the root
           WHEN N IN (select P from BST) THEN "Inner" // When a number is in both N and P, it means it has a parent node and it is a parent node too, so inner
